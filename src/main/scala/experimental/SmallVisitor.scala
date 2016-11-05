69c3cf1eacb18ab7dc4a2c9d8d42457e9d301a31
Tencho Tenev <tt1215@ic.ac.uk>
2016-11-05 20:37:08 +0000

    Clean up Norbert's code

--- src/main/scala/experimental/SmallVisitor.scala
+++ src/main/scala/experimental/SmallVisitor.scala
@@ -5,23 +5,22 @@ import antlr.WACCParserBaseVisitor
 
 class SmallVisitor[T] extends WACCParserBaseVisitor[T] {
 
-  var globalTable: SymbolTable = SymbolTable(Unit)
-  var currentTable: SymbolTable = Unit
+  var globalTable: SymbolTable = SymbolTable(None)
+  var currentTable: SymbolTable = globalTable
 
   override def visitAssignLhs(ctx: AssignLhsContext): T = super.visitAssignLhs(ctx)
 
   override def visitAssignRhs(ctx: AssignRhsContext): T = super.visitAssignRhs(ctx)
 
   override def visitAssign(ctx: AssignContext): T = {
-    val
     super.visitAssign(ctx)
   }
 
   override def visitProgram(ctx: ProgramContext): T = {
-    globalTable.addSymbol("int", new BaseType(min = -1000000, max = 10000000))
-    globalTable.addSymbol("bool", new BaseType(min = 0, max = 1))
-    globalTable.addSymbol("char", new BaseType(min = 0, max = 255))
-    globalTable.addSymbol("string", new BaseType(min = -1000000, max = 1000000))
+    globalTable.addSymbol("int", BaseType(min = -1000000, max = 10000000))
+    globalTable.addSymbol("bool", BaseType(min = 0, max = 1))
+    globalTable.addSymbol("char", BaseType(min = 0, max = 255))
+    globalTable.addSymbol("string", BaseType(min = -1000000, max = 1000000))
 
     super.visitProgram(ctx)
   }
