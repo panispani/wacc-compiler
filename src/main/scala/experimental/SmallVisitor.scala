c9524da0d46046eb9885191ab9c52d33147956b3
panpan2 <p.panayiotou2@gmail.com>
2016-11-07 12:49:38 +0000

    Visit program implementation

--- src/main/scala/experimental/SmallVisitor.scala
+++ src/main/scala/experimental/SmallVisitor.scala
@@ -2,29 +2,30 @@ package experimental
 
 import antlr.WACCParser._
 import antlr.WACCParserBaseVisitor
+import scala.collection.immutable.List
 
-class SmallVisitor[T] extends WACCParserBaseVisitor[T] {
+class SmallVisitor extends WACCParserBaseVisitor[Node] {
 
   var globalTable: SymbolTable = SymbolTable(None)
   var currentTable: SymbolTable = globalTable
 
   private def notInBounds(n: String): Boolean = {
     val num = BigInt(n)
-    return num > 2147483647 || num < -2147483648;
+    return num > 2147483647 || num < -2147483648
   }
 
-  override def visitIntLiteral(ctx: IntLiteralContext): T = {
+  override def visitIntLiteral(ctx: IntLiteralContext): Node = {
     if(notInBounds(ctx.NUMBER().toString)) {
       println("Runtime Error: Number is not in integer bounds")
     }
     super.visitIntLiteral(ctx)
   }
 
-  override def visitArrayType(ctx: ArrayTypeContext): T = super.visitArrayType(ctx)
+  override def visitArrayType(ctx: ArrayTypeContext): Node = super.visitArrayType(ctx)
 
-  override def visitPairType(ctx: PairTypeContext): T = super.visitPairType(ctx)
+  override def visitPairType(ctx: PairTypeContext): Node = super.visitPairType(ctx)
 
-  override def visitBaseType(ctx: BaseTypeContext): T = {
+  override def visitBaseType(ctx: BaseTypeContext): Node = {
     if (ctx.INT() != null) {
       //how? what is currrent variable
     }
@@ -33,32 +34,34 @@ class SmallVisitor[T] extends WACCParserBaseVisitor[T] {
     super.visitBaseType(ctx)
   }
 
-  override def visitType(ctx: TypeContext): T = super.visitType(ctx)
+  override def visitType(ctx: TypeContext): Node = super.visitType(ctx)
 
-  override def visitDeclare(ctx: DeclareContext): T = {
+  override def visitDeclare(ctx: DeclareContext): Node = {
     val ident = currentTable.lookup(ctx.IDENT().toString)
     if (ident.isDefined && ident.get.getType().toString != "function") {
       println("Semantic Error: Identifier is re-defined")
-      System.exit(200);
+      System.exit(200)
     }
     currentTable.addSymbol(ctx.IDENT().toString, new Identifier("variable"))
     super.visitDeclare(ctx)
   }
 
-  override def visitAssignLhs(ctx: AssignLhsContext): T = super.visitAssignLhs(ctx)
+  override def visitAssignLhs(ctx: AssignLhsContext): Node = super.visitAssignLhs(ctx)
 
-  override def visitAssignRhs(ctx: AssignRhsContext): T = super.visitAssignRhs(ctx)
+  override def visitAssignRhs(ctx: AssignRhsContext): Node = super.visitAssignRhs(ctx)
 
-  override def visitAssign(ctx: AssignContext): T = {
+  override def visitAssign(ctx: AssignContext): Node = {
     super.visitAssign(ctx)
   }
 
-  override def visitProgram(ctx: ProgramContext): T = {
-    globalTable.addSymbol("int", BaseType(min = -1000000, max = 10000000))
-    globalTable.addSymbol("bool", BaseType(min = 0, max = 1))
-    globalTable.addSymbol("char", BaseType(min = 0, max = 255))
-    globalTable.addSymbol("string", BaseType(min = -1000000, max = 1000000))
-
-    super.visitProgram(ctx)
+  /*  statements can't be empty
+   *
+   */
+  override def visitProgram(ctx: ProgramContext): Node = {
+    val childCount = ctx.getChildCount
+    val functions =
+      for (i <- List.range(0, childCount - 1)) yield visit(ctx.getChild(i)).asInstanceOf[FunctionNode]
+    val stmt = visit(ctx.getChild(childCount - 1)).asInstanceOf[StatementNode]
+    ProgramNode(functions, stmt)
   }
 }
