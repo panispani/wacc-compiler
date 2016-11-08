ae3928086f78eca8ae9bddd7961c2db9bbb2651e
Norbert Podsadowski <norbo11@googlemail.com>
2016-11-08 21:36:01 +0000

    Implemented semantic checking for ArrayElements

--- src/main/scala/wacc/visitors/ArrayElementVisitor.scala
+++ src/main/scala/wacc/visitors/ArrayElementVisitor.scala
@@ -1,8 +1,23 @@
 package wacc.visitors
 
+import antlr.WACCParser.ArrayElementContext
 import antlr.WACCParserBaseVisitor
-import wacc.constructs.ArrayElement
+import wacc.SymbolTable
+import wacc.constructs.{ArrayElement, Error, Function, Variable}
+import scala.collection.JavaConversions._
 
-object ArrayElementVisitor extends WACCParserBaseVisitor[ArrayElement] {
+import scala.util.{Failure, Success, Try}
+
+object ArrayElementVisitor extends WACCParserBaseVisitor[Try[ArrayElement]] {
+
+  override def visitArrayElement(ctx: ArrayElementContext): Try[ArrayElement] = {
+    val identifier = ctx.IDENT().toString
+
+    SymbolTable.currentTable.lookupAll(identifier) match {
+      case Some(variable: Variable)  => Success(ArrayElement(identifier, ctx.expression().toList map (_.accept(ExpressionVisitor))))
+      case None                      => Failure(Error("Semantic", "Variable not declared"))
+      case default                   => Failure(Error("Semantic" , "Identifier is a function, not an array variable"))
+    }
+  }
 
 }
