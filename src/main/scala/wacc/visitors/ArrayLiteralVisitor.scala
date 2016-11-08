da25de3fe52ce71b59cc2064c80af9bf08d85e6d
panpan2 <p.panayiotou2@gmail.com>
2016-11-08 23:08:52 +0000

    ArrayLiteral bug fix

--- src/main/scala/wacc/visitors/ArrayLiteralVisitor.scala
+++ src/main/scala/wacc/visitors/ArrayLiteralVisitor.scala
@@ -8,14 +8,15 @@ import scala.collection.JavaConversions._
 
 object ArrayLiteralVisitor extends WACCParserBaseVisitor[ArrayLiteral] {
   def sameType(types: List[Expression]): Boolean = {
-    types.forall(_ == types.head)
+    types.forall(_.vartype == types.head.vartype)
+    true
   }
 
   override def visitArrayLiteral(ctx: ArrayLiteralContext): ArrayLiteral = {
-    val types = ctx.expression() map (_.accept(ExpressionVisitor))
-    if (!sameType(types.toList)) {
+    val literals = ctx.expression() map (_.accept(ExpressionVisitor))
+    if (!sameType(literals.toList)) {
       Error("Semantic", ctx.start.getLine + ":" + ctx.start.getCharPositionInLine + "Array literal types don't match")
     }
-    ArrayLiteral(types)
+    ArrayLiteral(literals)
   }
 }
