2991e1480f36d188d654dab37ed1ec41ae31db01
panpan2 <p.panayiotou2@gmail.com>
2016-11-08 12:59:55 +0000

    Error construct to call on error

---
+++ src/main/scala/wacc/constructs/Error.scala
@@ -0,0 +1,18 @@
+package wacc.constructs
+
+/**
+  * Created by panayiotis on 08/11/16.
+  */
+case class Error(errorType: String, errorMessage: String) {
+  println(errorType + ": " + errorMessage)
+  val errorCode = getErrorCode(errorType)
+  if (errorCode != 0) {
+    System.exit(errorCode);
+  }
+
+  def getErrorCode(errorType: String): Int = errorType match {
+    case "Semantic"  => 200
+    case "Syntactic" => 100
+    case _           => 0
+  }
+}
