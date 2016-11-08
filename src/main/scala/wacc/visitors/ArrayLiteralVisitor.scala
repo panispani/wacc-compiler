3288656bf2e780d58297977d2c658a58bd238f9b
panpan2 <p.panayiotou2@gmail.com>
2016-11-08 22:50:55 +0000

    Array Literal visitor with semantic checking

--- src/main/scala/wacc/visitors/ArrayLiteralVisitor.scala
+++ src/main/scala/wacc/visitors/ArrayLiteralVisitor.scala
@@ -1,8 +1,21 @@
 package wacc.visitors
 
+import antlr.WACCParser.ArrayLiteralContext
 import antlr.WACCParserBaseVisitor
-import wacc.constructs.ArrayLiteral
+import wacc.constructs.{ArrayLiteral, Error, Expression}
+
+import scala.collection.JavaConversions._
 
 object ArrayLiteralVisitor extends WACCParserBaseVisitor[ArrayLiteral] {
+  def sameType(types: List[Expression]): Boolean = {
+    types.forall(_ == types.head)
+  }
 
+  override def visitArrayLiteral(ctx: ArrayLiteralContext): ArrayLiteral = {
+    val types = ctx.expression() map (_.accept(ExpressionVisitor))
+    if (!sameType(types.toList)) {
+      Error("Semantic", ctx.start.getLine + ":" + ctx.start.getCharPositionInLine + "Array literal types don't match")
+    }
+    ArrayLiteral(types)
+  }
 }
