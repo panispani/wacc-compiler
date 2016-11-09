260955512ab0ab41fbc309063c388c958da0c3bf
panpan2 <p.panayiotou2@gmail.com>
2016-11-09 19:27:48 +0000

    Literal Visitor responsibility split

--- src/main/scala/wacc/visitors/ExpressionVisitor.scala
+++ src/main/scala/wacc/visitors/ExpressionVisitor.scala
@@ -8,18 +8,6 @@ import wacc.visitor._
 
 object ExpressionVisitor extends WACCParserBaseVisitor[Either[CompilationError, Expression]] {
 
-  override def visitIntLiteral(ctx: IntLiteralContext): Either[CompilationError, Expression]
-  = Right(IntegerLiteral(ctx.getText.toInt))
-
-  override def visitBoolLiteral(ctx: BoolLiteralContext): Either[CompilationError, Expression]
-  = Right(BoolLiteral(ctx.getText.toBoolean))
-
-  override def visitCharLiteral(ctx: CharLiteralContext): Either[CompilationError, Expression]
-  = Right(CharLiteral(ctx.getText.charAt(1))) // 0 is a quote
-
-  override def visitStringLiteral(ctx: StringLiteralContext): Either[CompilationError, Expression]
-  = Right(StringLiteral(ctx.getText))
-
   override def visitVariableReference(ctx: VariableReferenceContext): Either[CompilationError, Expression] = {
     val identifier = ctx.IDENT().getText
     SymbolTable.currentTable.lookupAll(identifier) match {
@@ -28,10 +16,6 @@ object ExpressionVisitor extends WACCParserBaseVisitor[Either[CompilationError, 
       case Some(_: Typed) => Left(SemanticError("Identifier is not a variable"))
       case None => Left(SemanticError("Variable not declared : " + identifier))
     }
-  }
-
-  override def visitPairLiteral(ctx: PairLiteralContext): Either[CompilationError, Expression] = {
-    Right(PairLiteral(NullType, NullType, None))
   }
 
   override def visitBracketedExp(ctx: BracketedExpContext): Either[CompilationError, Expression] = {
@@ -44,11 +28,16 @@ object ExpressionVisitor extends WACCParserBaseVisitor[Either[CompilationError, 
     val unaryExpr = expr map (e => UnaryOperatorExpr(operator, e))
 
     operator match {
-      case MinusOp | ChrOp if expr map (e => e.vartype != Integer) => Left(SemanticError(operator.unaryOperator + " operator needs integers"))
-      case LenOp if expr map (e => e.vartype != ArrayType(Character)) => Left(SemanticError("'len' operator needs array of charcters"))
-      case OrdOp if expr map (e => e.vartype != Character) => Left(SemanticError("'ord' operator needs character"))
-      case NotOp if expr map (e => e.vartype != Boolean) => Left(SemanticError("'!' operator needs Boolean"))
-      case default => unaryExpr
+      case MinusOp | ChrOp if expr map (e => e.vartype != Integer) =>
+        Left(SemanticError(operator.unaryOperator + " operator needs integers"))
+      case LenOp if expr map (e => e.vartype != ArrayType(Character)) =>
+        Left(SemanticError("'len' operator needs array of charcters"))
+      case OrdOp if expr map (e => e.vartype != Character) =>
+        Left(SemanticError("'ord' operator needs character"))
+      case NotOp if expr map (e => e.vartype != Boolean) =>
+        Left(SemanticError("'!' operator needs Boolean"))
+      case default =>
+        unaryExpr
     }
   }
 
@@ -75,7 +64,8 @@ object ExpressionVisitor extends WACCParserBaseVisitor[Either[CompilationError, 
         if expr1.vartype != Boolean || expr2.vartype != Boolean =>
         SemanticError(operator.binaryOperator + " operator needs 2 booleans as its arguments")
 
-      case default => BinaryOperatorExpr(expr1, operator, expr2)
+      case default =>
+        BinaryOperatorExpr(expr1, operator, expr2)
     }
   }
 }
--- src/main/scala/wacc/visitors/LiteralVisitor.scala
+++ src/main/scala/wacc/visitors/LiteralVisitor.scala
@@ -1,12 +1,26 @@
 package wacc.visitors
 
-import antlr.WACCParser.LiteralContext
+import antlr.WACCParser._
 import antlr.WACCParserBaseVisitor
-import wacc.constructs.Literal
+import wacc.constructs.{CharLiteral, StringLiteral, _}
 
 /**
   * Created by panayiotis on 08/11/16.
   */
 object LiteralVisitor extends WACCParserBaseVisitor[Literal] {
-  override def visitLiteral(ctx: LiteralContext): Literal = super.visitLiteral(ctx)
+  override def visitIntLiteral(ctx: IntLiteralContext): Either[CompilationError, Expression]
+    = Right(IntegerLiteral(ctx.getText.toInt))
+
+  override def visitBoolLiteral(ctx: BoolLiteralContext): Either[CompilationError, Expression]
+    = Right(BoolLiteral(ctx.getText.toBoolean))
+
+  override def visitCharLiteral(ctx: CharLiteralContext): Either[CompilationError, Expression]
+    = Right(CharLiteral(ctx.getText.charAt(1))) // 0 is a quote
+
+  override def visitStringLiteral(ctx: StringLiteralContext): Either[CompilationError, Expression]
+    = Right(StringLiteral(ctx.getText))
+
+  override def visitPairLiteral(ctx: PairLiteralContext): Either[CompilationError, Expression]
+    = Right(PairLiteral(NullType, NullType, None))
+
 }
