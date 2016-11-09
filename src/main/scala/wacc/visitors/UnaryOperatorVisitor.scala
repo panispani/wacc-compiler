6081bd27dc9c275249dbfdf8228c144189cb88f4
panpan2 <p.panayiotou2@gmail.com>
2016-11-09 00:03:42 +0000

    Unary operator visitor and construct

---
+++ src/main/scala/wacc/visitors/UnaryOperatorVisitor.scala
@@ -0,0 +1,12 @@
+package wacc.visitors
+
+import antlr.WACCParser.UnaryOperatorContext
+import antlr.{WACCParser, WACCParserBaseVisitor}
+import wacc.constructs.UnaryOperator
+
+/**
+  * Created by panayiotis on 08/11/16.
+  */
+object UnaryOperatorVisitor extends WACCParserBaseVisitor[UnaryOperator] {
+
+}
