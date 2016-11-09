d95706d8a48007f328192fa5d040690a60dcc2bf
Tencho Tenev <tt1215@ic.ac.uk>
2016-11-09 12:48:26 +0000

    Change blocks to one-liners

--- src/main/scala/wacc/visitors/ExpressionVisitor.scala
+++ src/main/scala/wacc/visitors/ExpressionVisitor.scala
@@ -6,13 +6,11 @@ import wacc.constructs._
 
 object ExpressionVisitor extends WACCParserBaseVisitor[Either[CompilationError, Expression]] {
 
-  override def visitIntLiteral(ctx: IntLiteralContext): Either[CompilationError, Expression] = {
-    Right(IntegerLiteral(ctx.getText.toInt))
-  }
+  override def visitIntLiteral(ctx: IntLiteralContext): Either[CompilationError, Expression]
+    = Right(IntegerLiteral(ctx.getText.toInt))
 
-  override def visitBoolLiteral(ctx: BoolLiteralContext): Either[CompilationError, Expression] = {
-    Right(BoolLiteral(ctx.getText.toBoolean))
-  }
+  override def visitBoolLiteral(ctx: BoolLiteralContext): Either[CompilationError, Expression]
+    = Right(BoolLiteral(ctx.getText.toBoolean))
 
   override def visitCharLiteral(ctx: CharLiteralContext): Either[CompilationError, Expression]
     = Right(CharLiteral(ctx.getText.charAt(1))) // 0 is a quote
