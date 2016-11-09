435bb189c994dbb764db96a736e17bbaf6fc4f27
Tencho Tenev <tt1215@ic.ac.uk>
2016-11-09 02:15:41 +0000

    More meaningful structure for compilation errors

--- src/main/scala/wacc/constructs/CompilationError.scala
+++ src/main/scala/wacc/constructs/CompilationError.scala
@@ -1,18 +1,14 @@
 package wacc.constructs
 
-/**
-  * Created by panayiotis on 08/11/16.
-  */
-case class Error(errorType: String, errorMessage: String) extends Throwable {
-  println(errorType + ": " + errorMessage)
-  val errorCode = getErrorCode(errorType)
-  if (errorCode != 0) {
-    System.exit(errorCode)
-  }
 
-  def getErrorCode(errorType: String): Int = errorType match {
-    case "Semantic"  => 200
-    case "Syntactic" => 100
-    case _           => 0
-  }
+abstract class CompilationError(val message: String) {
+  def raise(): Unit = println(message)
+}
+
+case class SyntaxError(override val message: String) extends CompilationError(message) {
+  override def raise(): Unit = super.raise(); System.exit(100)
+}
+
+case class SemanticError(override val message: String) extends CompilationError(message) {
+  override def raise(): Unit = super.raise(); System.exit(200)
 }
