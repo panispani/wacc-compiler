aa2ad719dfa22483ad3e2a925ccd9e3970d26c1f
Norbert Podsadowski <norbo11@googlemail.com>
2016-11-09 18:21:52 +0000

    Implemented pair constructors

--- src/main/scala/wacc/visitors/PairConstructorVisitor.scala
+++ src/main/scala/wacc/visitors/PairConstructorVisitor.scala
@@ -1,6 +1,17 @@
 package wacc.visitors
 
+import antlr.WACCParser.{PairConstructorContext, AssignRhsPairConstructorContext}
 import antlr.WACCParserBaseVisitor
-import wacc.constructs.PairConstructor
+import wacc.constructs.{CompilationError, PairConstructor}
 
-object PairConstructorVisitor extends WACCParserBaseVisitor[PairConstructor]
+object PairConstructorVisitor extends WACCParserBaseVisitor[Either[CompilationError, PairConstructor]] {
+
+  override def visitPairConstructor(ctx: PairConstructorContext): Either[CompilationError, PairConstructor] = {
+
+    for {
+      exp1 <- ctx.expression1.accept(ExpressionVisitor).right
+      exp2 <- ctx.expression2.accept(ExpressionVisitor).right
+    } yield PairConstructor(exp1, exp2)
+
+  }
+}
