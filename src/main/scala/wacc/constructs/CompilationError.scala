396e78b207ae988385398859721ebc2a76e7c1eb
Tencho Tenev <tt1215@ic.ac.uk>
2016-11-09 12:45:00 +0000

    Increase abstraction for error classes

--- src/main/scala/wacc/constructs/CompilationError.scala
+++ src/main/scala/wacc/constructs/CompilationError.scala
@@ -2,19 +2,17 @@ package wacc.constructs
 
 
 abstract class CompilationError(val message: String) {
-  def raise(): Unit = println(message)
+  def raise(): Unit = {
+    println(exitCode + " : " + message)
+    System.exit(exitCode)
+  }
+  val exitCode : Int
 }
 
 case class SyntaxError(override val message: String) extends CompilationError(message) {
-  override def raise(): Unit = {
-    super.raise()
-    System.exit(100)
-  }
+  override val exitCode: Int = 100
 }
 
 case class SemanticError(override val message: String) extends CompilationError(message) {
-  override def raise(): Unit = {
-    super.raise()
-    System.exit(200)
-  }
+  override val exitCode: Int = 200
 }
