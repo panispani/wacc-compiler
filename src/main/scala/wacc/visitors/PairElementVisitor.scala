8d4ffdd2e1dcd5591e755d3e1056629b6919f416
Norbert Podsadowski <norbo11@googlemail.com>
2016-11-10 14:57:55 +0000

    Moved array element visitor under expression visitor

--- src/main/scala/wacc/visitors/PairElementVisitor.scala
+++ src/main/scala/wacc/visitors/PairElementVisitor.scala
@@ -6,11 +6,11 @@ import wacc.constructs._
 import wacc.util.SemanticErrors
 
 object PairElementVisitor extends WACCParserBaseVisitor[Either[CompilationError, PairElement]] {
-
   override def visitPairElement(ctx: PairElementContext): Either[CompilationError, PairElement] = {
     ctx.expression().accept(ExpressionVisitor).right flatMap (e => e.vartype match {
       case PairType(_, _)       => Right(PairElement(Selector(ctx.selector.getText), e))
       case default              => Left(SemanticError("Pair element " + SemanticErrors.typeError("expression", e.vartype.toString, PairType.toString())))
     })
   }
+
 }
