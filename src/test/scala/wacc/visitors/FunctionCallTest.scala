1abd17b1be925dc0d2198a920b890e014b198147
panpan2 <p.panayiotou2@gmail.com>
2016-11-10 23:47:05 +0000

    Add more tests for function calls

--- src/test/scala/wacc/visitors/FunctionCallTest.scala
+++ src/test/scala/wacc/visitors/FunctionCallTest.scala
@@ -34,4 +34,40 @@ class FunctionCallTest extends VisitorTest{
       )
   }
 
+  it should "succeed when the argument types match" in {
+    val parser = TestUtilities.setupParser("begin int foo(int a) is return a end int a = call foo(2) end")
+    val result = TestUtilities.buildSubProgram(parser.program, ProgramVisitor)
+
+    result.right.value should be(
+      Program(
+        List(
+          Function(
+            "foo",
+            List(Param(Variable("a",PrimitiveType("int")))),
+            PrimitiveType("int"),
+            List(
+              ReturnStatement(VariableReferenceExpression(PrimitiveType("int"))))
+            )
+          ),
+          List(
+            DeclareStatement(
+              PrimitiveType("int"),
+              "a",
+              FunctionCall(
+                "foo",
+                List(IntegerLiteral(2)),
+                PrimitiveType("int")
+              )
+            )
+          )
+        )
+    )
+  }
+
+  it should "fail when argument types don't match" in {
+    val parser = TestUtilities.setupParser("begin int foo(int a) is return a end int a = call foo('a') end")
+    val result = TestUtilities.buildSubProgram(parser.program, ProgramVisitor)
+
+    result.left.value shouldBe a[SemanticError]
+  }
 }
