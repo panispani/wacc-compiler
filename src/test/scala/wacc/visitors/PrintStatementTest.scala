51e191b8dc52b6a2c886ace059fb53d1740864d4
panpan2 <p.panayiotou2@gmail.com>
2016-11-10 14:37:34 +0000

    Split concerns in Tests

---
+++ src/test/scala/wacc/visitors/PrintStatementTest.scala
@@ -0,0 +1,30 @@
+package wacc.visitors
+
+import wacc.constructs.{CharLiteral, IntegerLiteral, PrintStatement, StringLiteral}
+
+/**
+  * Created by panayiotis on 10/11/16.
+  */
+class PrintStatementTest extends VisitorTest {
+
+  "Visiting a print statement" should "allow int arguments" in {
+    val parser = TestUtilities.setupParser("print 3")
+    val result = TestUtilities.buildSubProgram(parser.statement, StatementVisitor)
+
+    result.right.value should be (PrintStatement(IntegerLiteral(3)))
+  }
+
+  "Visiting a print statement" should "allow char arguments" in {
+    val parser = TestUtilities.setupParser("print 'a'")
+    val result = TestUtilities.buildSubProgram(parser.statement, StatementVisitor)
+
+    result.right.value should be (PrintStatement(CharLiteral('a')))
+  }
+
+  "Visiting a print statement" should "allow string arguments" in {
+    val parser = TestUtilities.setupParser("print \"str\"")
+    val result = TestUtilities.buildSubProgram(parser.statement, StatementVisitor)
+
+    result.right.value should be (PrintStatement(StringLiteral("str")))
+  }
+}
---
+++ src/test/scala/wacc/visitors/ScopeStatementTest.scala
@@ -0,0 +1,49 @@
+package wacc.visitors
+
+import wacc.constructs.{SemanticError, _}
+
+/**
+  * Created by panayiotis on 10/11/16.
+  */
+class ScopeStatementTest extends VisitorTest {
+  "Visiting a new scope" should "allow re-declarations in new scopes" in {
+    val parser = TestUtilities.setupParser("int a = 1; begin int a = 2 end")
+    val result = TestUtilities.buildSubProgram(parser.sequence, SequenceVisitor)
+
+    result.right.value should be (
+      List(
+        DeclareStatement(Integer,"a",IntegerLiteral(1)),
+        ScopeStatement(
+          List(
+            DeclareStatement(Integer, "a",IntegerLiteral(2))
+          )
+        )
+      )
+    )
+  }
+
+  it should "use parent variables after exiting a scope" in {
+    val parser = TestUtilities.setupParser("int a = 1; begin int a = 2 end; int b = a")
+    val result = TestUtilities.buildSubProgram(parser.sequence, SequenceVisitor)
+
+    result.right.value should be (
+      List(
+        DeclareStatement(Integer,"a",IntegerLiteral(1)),
+        ScopeStatement(
+          List(
+            DeclareStatement(Integer, "a",IntegerLiteral(2))
+          )
+        ),
+        DeclareStatement(Integer,"b",VariableReferenceExpression(Integer))
+      )
+    )
+  }
+
+  it should "use parent variables when exiting a scope and not allow redeclarations" in {
+    val parser = TestUtilities.setupParser("int a = 1; begin int a = 2 end; int a = 3")
+    val result = TestUtilities.buildSubProgram(parser.sequence, SequenceVisitor)
+
+    result.left.value should be (SemanticError("Re-declaration of variable a"))
+  }
+
+}
