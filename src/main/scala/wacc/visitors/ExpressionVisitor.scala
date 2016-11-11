5c4e958a43bc7b1bb70a5274bc97a6cc2b6e0d01
Tencho Tenev <tt1215@ic.ac.uk>
2016-11-11 10:32:33 +0000

    Use compatible types check in equals operators

--- src/main/scala/wacc/visitors/ExpressionVisitor.scala
+++ src/main/scala/wacc/visitors/ExpressionVisitor.scala
@@ -73,8 +73,8 @@ object ExpressionVisitor extends WACCParserBaseVisitor[Either[CompilationError, 
           Left(SemanticError(operator.binaryOperator + " operator needs 2 integers/characters as its arguments"))
 
         case EqualsBinOp | NequalsBinOp
-        if expr1.vartype != expr2.vartype =>
-          Left(SemanticError(operator.binaryOperator + " operator needs 2 arguments of the same type"))
+        if !compatibleTypes(expr1.vartype, expr2.vartype) =>
+          Left(SemanticError(operator.binaryOperator + " operator needs 2 arguments of a compatible type"))
 
         case AndBinOp | OrBinOp
         if expr1.vartype != Boolean || expr2.vartype != Boolean =>
