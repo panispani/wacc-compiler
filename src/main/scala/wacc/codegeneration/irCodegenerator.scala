d453a7073bb01cd8baeb2fa434e1d70adc47a738
panpan2 <p.panayiotou2@gmail.com>
2016-11-15 19:45:29 +0000

    codegenerator with pattern matching on input AST

--- src/main/scala/wacc/codegeneration/irCodegenerator.scala
+++ src/main/scala/wacc/codegeneration/irCodegenerator.scala
@@ -1,6 +1,6 @@
 package wacc.codegeneration
 
-import wacc.constructs.Program
+import wacc.constructs._
 
 /**
   * Created by panayiotis on 15/11/16.
@@ -12,8 +12,15 @@ trait Instruction
 case class Add(Rd: Register, Rn: Register, Op2: Operand) extends Instruction
 
 class irCodegenerator {
-  def codegen(program: Program): Seq[Instruction] = {
-    println("codegen")
+  def codegen(a: Any): Seq[Instruction] = {
+    a match {
+      case Program(functions, instructions) => for (instr <- instructions) codegen(instr)
+      case Function(ident, params, vartype, stmt) => ;
+      case IntegerLiteral(value: Integer) => ;
+      case DeclareStatement(vartype: Type, identifier: String, value: AssignValue) => println("declare");
+      case default => println("nope")
+    }
+
     Seq[Instruction]()
   }
 }
--- src/test/scala/wacc/codegeneration/irCodegenTest.scala
+++ src/test/scala/wacc/codegeneration/irCodegenTest.scala
@@ -1,11 +1,10 @@
 package wacc.codegeneration
 
 import wacc.constructs._
-import wacc.codegeneration.irCodegenerator
 
 class irCodegenTest extends CodeGenTest {
   "Running a test" should "be possible" in {
-    val program = Program(Seq(), Seq())
+    val program = Program(Seq(), Seq(DeclareStatement(PrimitiveType("int"),"x",IntegerLiteral(1))))
     val ircodegen = new irCodegenerator
     ircodegen.codegen(program)
   }
