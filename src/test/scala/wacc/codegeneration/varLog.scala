48d0d8ae22d563a0cc849137cba8551bde64c57a
panpan2 <p.panayiotou2@gmail.com>
2016-11-16 22:13:06 +0000

    Introduce experimental varLog to keep track of variables

---
+++ src/test/scala/wacc/codegeneration/varLog.scala
@@ -0,0 +1,29 @@
+package wacc.codegeneration
+
+import wacc.constructs.{ArrayType, PairType, PrimitiveType, Type}
+
+/**
+  * Created by panayiotis on 16/11/16.
+  */
+class varLog {
+  private var bytes: Integer = 0
+  private var vars: Integer = 0
+  def add(name: String, location: Integer, vartype: Type): Unit = {
+    val varsize = vartype match {
+      case PrimitiveType("int") => 4
+      case PrimitiveType("bool") => 1
+      case PrimitiveType("char") => 1
+      case PrimitiveType("string") => 4 //keep on heap, this is a pointer
+      case ArrayType(elemtype: Type) => 4 //keep on heap
+      case PairType(ftype, sType) => 4 //keep on heap
+    }
+    vars = vars + 1
+    bytes = bytes + varsize
+  }
+  def varCount(): Integer = {
+    vars
+  }
+  def byteCount(): Integer = {
+    bytes
+  }
+}
