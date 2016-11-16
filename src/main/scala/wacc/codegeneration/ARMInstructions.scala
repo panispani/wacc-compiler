e5a949a689096bbe339e72ad1740f5ba5a8ae76d
panpan2 <p.panayiotou2@gmail.com>
2016-11-16 15:58:10 +0000

    Adding ARM instruction case classes

--- src/main/scala/wacc/codegeneration/ARMInstructions.scala
+++ src/main/scala/wacc/codegeneration/ARMInstructions.scala
@@ -4,6 +4,23 @@ package wacc.codegeneration
   * Created by panayiotis on 15/11/16.
   */
 trait Instruction
-trait Operand
-case class Add(Rd: Register, Rn: Register, Op2: Operand) extends Instruction
-
+case class ADC(Rd: Register, Rn: Register, Op2: Operand) extends Instruction
+case class ADD(Rd: Register, Rn: Register, Op2: Operand) extends Instruction
+case class ADDS(Rd: Register, Rn: Register, i12: Imm12) extends Instruction
+case class AND(Rd: Register, Rn: Register, Op2: Operand) extends Instruction
+case class BRANCH() extends Instruction
+case class CMP(Rn: Register, Op2: Operand) extends Instruction
+case class EOR(Rn: Register, Op2: Operand) extends Instruction
+case class LDR(Rt: Register, Rn: Register, offset: Integer) extends Instruction // use stack pointer
+case class MOV(Rd: Register, Op2: Operand) extends Instruction
+case class MOVS(Rd: Register, i16: Imm16) extends Instruction
+case class MUL(Rd: Register, Rn: Register, Rm: Register) extends Instruction
+case class MULS(Rn: Register, Rm: Register) extends Instruction
+case class ORR(Rd: Register, Rn: Register, Op2: Operand)
+case class SUB(Rd: Register, Rn: Register, Op2: Operand)
+case class SUBS(Rn: Register, Op2: Operand)
+case class SUB2(Rd: Register, Rn: Register, i12: Imm12)
+case class SUB2S(Rn: Register, i12: Imm12)
+case class STR(Rt: Register, Rn: Register, offset: Integer) extends Instruction
+case class PUSH(reglist: Seq[Register])
+case class POP(reglist: Seq[Register])
