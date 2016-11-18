68da28f119a8a7cc21ff0649767f7e40c1eff840
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2016-11-18 15:47:33 +0000

    Create separate file for exit statement tests

---
+++ src/test/scala/wacc/codegeneration/ExitStatementTest.scala
@@ -0,0 +1,17 @@
+package wacc.codegeneration
+
+import wacc.TransStatements._
+import wacc.constructs.{CompilationError, Statement}
+import wacc.visitors.{StatementVisitor, TestUtilities}
+
+class ExitStatementTest extends  CodeGenTest {
+
+  "Exiting" should "produce the expected instructions" in {
+    val parser = TestUtilities.setupParser("exit 7")
+    val program: Either[CompilationError, Statement] = TestUtilities.buildSubProgram(parser.statement, StatementVisitor)
+    val availableRegisters = Seq(R4, R5, R6)
+    val instructions = transStatement(program.right.get, availableRegisters)
+    println(instructions)
+    instructions shouldBe Seq(MOV(R4, ImmOperand(7)), MOV(R0, RegisterOperand(R4)), BL(Label("exit")))
+  }
+}
