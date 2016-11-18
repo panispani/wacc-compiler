9a29d30d3c5f37fdf38853ab0f4123d0a60bdfc6
panpan2 <p.panayiotou2@gmail.com>
2016-11-18 15:44:13 +0000

    First attempt for script that runs all valid programs

---
+++ run_programs.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+run_wacc_files() {
+    FILES=$(ls)
+    for f in $FILES
+    do
+        extension=${f##*.}
+        if [ -d $f ]
+        then
+            cd $f
+            test_wacc_files
+            cd ".."
+        elif [ $extension = "wacc" ]
+        then
+            $BASE_DIR/compile $f>"file.s"
+            arm-linux-gnueabi-gcc -o FILENAME1 -mcpu=arm1176jzf-s -mtune=arm1176jzf-s "file.s"
+            actual=$(qemu-arm -L /usr/arm-linux-gnueabi/ FILENAME1)
+
+            #find_output
+            while read line; do
+                  if [ line -eq "# Output:" ]
+                  then
+                    read line
+                    if [ line -eq "# #empty#" ]
+                    then
+                        $expected=""
+                    else
+                        $prefix="# "
+                        $expected=${string#$prefix}
+                    fi
+                    break
+                  fi
+                  echo $line
+            done <"file.s"
+
+            expected=find_output $f
+
+            if [ $actual -eq $expected ]
+            then
+                correct=$((correct+1))
+            else
+                echo "error for "$f
+            fi
+            total=$((total+1))
+        fi
+    done
+}
+
+if [ $# -eq 0 ]
+then
+    make
+fi
+BASE_DIR=$(pwd)
+echo "Running valid programs"
+cd "wacc_examples/valid"
+correct=0
+total=0
+run_wacc_files
+echo "TOTAL FILES: "$total
+echo "CORRECT    : "$correct
+
+
+
+
