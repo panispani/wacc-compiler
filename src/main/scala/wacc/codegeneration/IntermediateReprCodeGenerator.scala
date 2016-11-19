3ed4273a6e9afbb9d9579f4489cf660c61cf1f65
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2016-11-19 18:27:29 +0000

    Improve name of class and method

--- src/main/scala/wacc/codegeneration/IntermediateReprCodeGenerator.scala
+++ src/main/scala/wacc/codegeneration/IntermediateReprCodeGenerator.scala
@@ -7,10 +7,10 @@ import wacc.TransPrograms._
   * Created by panayiotis on 15/11/16.
   */
 //accumulator register approach
-class IrCodegenerator {
+class IntermediateReprCodeGenerator {
   val allRegisters = Seq(R0, R1, R2, R3, R4, R5, R6, R7, R8, R9, R10, R11, R12)
 
-  def codegen(program: Program): Seq[Instruction] = {
+  def generateCode(program: Program): Seq[Instruction] = {
     transNext(program, allRegisters)
   }
 
