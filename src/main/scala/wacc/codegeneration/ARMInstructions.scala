79697138d7407c42c4dfd15e9ebeb69d4cca1d57
Norbert Podsadowski <norbo11@googlemail.com>
2016-11-21 21:50:15 +0000

    Introduce data declaration instructions

--- src/main/scala/wacc/codegeneration/ARMInstructions.scala
+++ src/main/scala/wacc/codegeneration/ARMInstructions.scala
@@ -86,5 +86,18 @@ case class STRB(Rt: Register, address: Address) extends Instruction
 case class PUSH(reglist: Seq[Register]) extends Instruction // up to 8
 case class POP(reglist: Seq[Register]) extends Instruction
 
-object RETURN extends MOV(PC, RegisterOperand(LR))
+object RETURN extends MOV(PC, LR)
 object NEW_STACK_FRAME extends PUSH(Seq(LR))
+
+// Data
+trait DataInstruction extends Instruction {
+  def size = 0
+}
+
+case class AsciiData(labelAddress: LabelAddress, data: String) extends DataInstruction {
+  override def size = 4 * data.length
+}
+
+case class IntegerData(labelAddress: LabelAddress, data: Int) extends DataInstruction {
+  override def size = 4
+}
