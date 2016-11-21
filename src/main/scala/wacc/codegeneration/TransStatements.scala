b471cbde0cf356ed463ef0cc277aabf37bc9d805
panpan2 <p.panayiotou2@gmail.com>
2016-11-21 12:44:43 +0000

    Conditional bug fix, second label should at loop exit, added unit test that verifies it

--- src/main/scala/wacc/codegeneration/TransStatements.scala
+++ src/main/scala/wacc/codegeneration/TransStatements.scala
@@ -96,12 +96,11 @@ package object TransStatements {
 
     transExpression(expression, registers) ++
       Seq(CMP(registers.head, ImmOperand(0)),
-          B(L0, EQ()),
-          B(L1),
-          DefineLabel(L0)) ++
+          B(L0, EQ())) ++
       transStatementSequence(falseStatements, registers) ++
-      Seq(DefineLabel(L1)) ++
-      transStatementSequence(trueStatements, registers)
+      Seq(B(L1), DefineLabel(L0)) ++
+      transStatementSequence(trueStatements, registers) ++
+      Seq(DefineLabel(L1))
   }
 
   def transStatementSequence(seq: Seq[Statement], registers: Seq[Register]): Seq[Instruction] = {
