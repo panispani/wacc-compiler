8add0d35932ebc6dd52e5eb7a76f03e086c87f59
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2016-11-22 23:57:45 +0000

    Change content of string representing divide by zero error

--- src/main/scala/wacc/codegeneration/StaticCode.scala
+++ src/main/scala/wacc/codegeneration/StaticCode.scala
@@ -32,7 +32,7 @@ package object StaticCode {
   def printFormatLabel: Label = Label("print_format")
   def emptyStringLabel: Label = Label("empty_string")
   def throwRuntimeErrorLabel: Label = Label("throw_runtime_error")
-  def DivideByZeroErrorLabel: Label = Label("throw_runtime_error")
+  def DivideByZeroErrorLabel: Label = Label("divide_by_zero")
   def checkDivideByZeroLabel: Label = Label("check_divide_by_zero")
   def divisionLabel: Label = Label("__aeabi_idiv")
   def moduleLabel: Label = Label("__aeabi_idivmod")
