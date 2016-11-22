8f97d72496816b47d5c35282b3c3546d240117df
Norbert Podsadowski <norbo11@googlemail.com>
2016-11-22 20:20:54 +0000

    Implemented comment and newline output to assembly files

--- src/main/scala/wacc/arm/ARMHelpers.scala
+++ src/main/scala/wacc/arm/ARMHelpers.scala
@@ -2,3 +2,11 @@ package wacc.arm
 
 object RETURN extends MOV(PC, LR)
 object NEW_STACK_FRAME extends PUSH(Seq(LR))
+
+case class COMMENT(string: String) extends Instruction {
+  override val name = s"# $string"
+}
+
+object NEWLINE extends Instruction {
+  override val name = ""
+}
