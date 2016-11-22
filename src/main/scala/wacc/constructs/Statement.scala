c540086b2e9c108f35154444fe64558f833b7831
Tencho Tenev <tt1215@ic.ac.uk>
2016-11-22 13:13:13 +0000

    Calrify usage of VariableReference in DeclareStatement

--- src/main/scala/wacc/constructs/Statement.scala
+++ src/main/scala/wacc/constructs/Statement.scala
@@ -14,7 +14,11 @@ case class ReadStatement(target: AssignTarget) extends Statement
 case class SkipStatement() extends Statement
 case class ConditionalStatement(expression: Expression, trueStatements: Seq[Statement], falseStatements: Seq[Statement],
                                 symbolTable: SymbolTable) extends Statement
-case class DeclareStatement(vartype: Type, identifier: VariableReference, value: AssignValue) extends Statement
+
+// Identifier is a new reference here so it will always have the correct offset at parse-time
+// Otherwise we would need to have it as a String and do additional lookup during code generation
+case class DeclareStatement(vartype: Type, newReference: VariableReference, value: AssignValue) extends Statement
+
 case class LoopStatement(condition: Expression, statements: Seq[Statement], symbolTable: SymbolTable) extends Statement
 
 
