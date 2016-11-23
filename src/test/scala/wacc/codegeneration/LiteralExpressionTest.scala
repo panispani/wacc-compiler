35fa0d775f1ce3ed64c03c635d2dd930953530b3
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2016-11-23 16:57:07 +0000

    Create literal expression traslation test for pair literal

--- src/test/scala/wacc/codegeneration/LiteralExpressionTest.scala
+++ src/test/scala/wacc/codegeneration/LiteralExpressionTest.scala
@@ -4,7 +4,7 @@ import org.scalatest.{FlatSpec, Matchers}
 import wacc.SymbolTable
 import wacc.TransExpressions._
 import wacc.arm._
-import wacc.constructs.IntegerLiteral
+import wacc.constructs.{IntegerLiteral, PairLiteral}
 /**
   * Created by tt1215 on 18/11/16.
   */
@@ -18,4 +18,11 @@ class LiteralExpressionTest extends CodeGenTest {
     instructions.head should be (MOV(R4, ImmOperand(1)))
   }
 
+  "The value of an expression consinsting of pair literal" should "be stored in the first available register" in {
+    val availableRegisters = Seq(R4, R5, R6)
+    val instructions = transExpression(PairLiteral(), SymbolTable.globalTable, availableRegisters)
+
+    instructions.size should be (1)
+    instructions.head should be (MOV(R4, ImmOperand(0)))
+  }
 }
