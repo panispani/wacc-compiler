69154eab7a4b35aa60a30417909a38b8e869c7a0
panpan2 <p.panayiotou2@gmail.com>
2016-11-23 17:33:33 +0000

    Use LDR instead of MOV to load from LABELS

--- src/main/scala/wacc/codegeneration/TransExpressions.scala
+++ src/main/scala/wacc/codegeneration/TransExpressions.scala
@@ -37,7 +37,7 @@ package object TransExpressions {
       case IntegerLiteral(value) => Seq(MOV(reg1, ImmOperand(value)))
       case BoolLiteral(value)    => Seq(MOV(reg1, ImmOperand(if (value) 1 else 0)))
       case CharLiteral(value)    => Seq(MOV(reg1, CharOperand(value)))
-      case StringLiteral(value)  => Seq(MOV(reg1, LabelAddress(DefineStringLabel(value).label)))
+      case StringLiteral(value)  => Seq(LDR(reg1, LabelAddress(DefineStringLabel(value).label)))
       case PairLiteral()         => Seq(MOV(reg1, ImmOperand(0)))
     }
   }
