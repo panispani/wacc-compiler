4982a869fc01d8eddd5a5242fd75ce4069f16ef2
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2016-11-23 01:12:43 +0000

    Ovveride toString for NEW_STACK_FRAME

--- src/main/scala/wacc/arm/ARMHelpers.scala
+++ src/main/scala/wacc/arm/ARMHelpers.scala
@@ -3,7 +3,10 @@ package wacc.arm
 object RETURN extends POP(Seq(PC)) {
   override val name = "POP"
 }
-object NEW_STACK_FRAME extends PUSH(Seq(LR))
+
+object NEW_STACK_FRAME extends PUSH(Seq(LR)) {
+  override val name = "PUSH"
+}
 
 case class COMMENT(string: String) extends Instruction {
   override val name = s"# $string"
