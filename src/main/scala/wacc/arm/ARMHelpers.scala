d8e1baae65b28ac9aefd1e2b32cece3351ccf38e
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2016-11-23 01:10:54 +0000

    Ovveride toString for RETURN

--- src/main/scala/wacc/arm/ARMHelpers.scala
+++ src/main/scala/wacc/arm/ARMHelpers.scala
@@ -1,6 +1,8 @@
 package wacc.arm
 
-object RETURN extends POP(Seq(PC))
+object RETURN extends POP(Seq(PC)) {
+  override val name = "POP"
+}
 object NEW_STACK_FRAME extends PUSH(Seq(LR))
 
 case class COMMENT(string: String) extends Instruction {
