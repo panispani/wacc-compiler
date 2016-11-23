c429f95730348487202d91be2a5bc07808c41ae4
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2016-11-23 18:58:00 +0000

    Refacotr unary operator translation

--- src/main/scala/wacc/codegeneration/TransUnaryOperators.scala
+++ src/main/scala/wacc/codegeneration/TransUnaryOperators.scala
@@ -7,15 +7,15 @@ package object TransUnaryOperators {
 
   def transUnaryOperator(r: Register, op: UnaryOperator): Seq[Instruction] = {
     op match {
-      case LenOp  => Seq(LDR(r, RegisterAddress(r, 0)))
+      case NotOp   => Seq(EOR(r, r, ImmOperand(1)))
 
       case MinusOp => Seq(RSBS(r, r, ImmOperand(0)))
 
-      case NotOp => Seq(EOR(r, r, ImmOperand(1)))
+      case LenOp   => Seq(LDR(r, RegisterAddress(r, 0)))
 
-      case OrdOp => Seq()
+      case OrdOp   => Seq()
 
-      case ChrOp => Seq()
+      case ChrOp   => Seq()
     }
   }
 
