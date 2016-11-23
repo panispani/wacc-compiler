80747bb751703ad9cba7d0fafcc056274a22a59d
Tencho Tenev <tt1215@imperial.ac.uk>
2016-11-23 15:53:16 +0000

    Add macro for setting up a frame

--- src/main/scala/wacc/arm/Macros.scala
+++ src/main/scala/wacc/arm/Macros.scala
@@ -1,8 +1,39 @@
 package wacc.arm
 
-/**
-  * Created by tt1215 on 23/11/16.
-  */
-class Macros {
+import wacc.codegeneration.CodeSegment
 
+object Macros {
+
+  /**
+    * Returns the code for opening and closing a scope
+    * If the scope is a function call the isBranch flag must be set to true
+    * so that the LR and PC are handled appropriately
+    * */
+  def frame(size: Int, isBranch: Boolean = false): (CodeSegment, CodeSegment) = {
+    val MAX_SIZE = 1024
+
+    val start = new CodeSegment()
+
+    if (isBranch) start.append(PUSH(Seq(LR)))
+    start.extend(Seq(PUSH(Seq(FP)), MOV(FP, SP)))
+
+    val end = new CodeSegment()
+
+    // Handle large scopes by adding / subtracting several times
+    val blocks = size / MAX_SIZE
+    val remainder = size % MAX_SIZE
+
+    for (i <- 0 to blocks) {
+      start.append(SUB(SP, SP, ImmOperand(MAX_SIZE)))
+      end.append(ADD(SP, SP, ImmOperand(MAX_SIZE)))
+    }
+
+    start.append(SUB(SP, SP, ImmOperand(remainder)))
+    end.append(ADD(SP, SP, ImmOperand(remainder)))
+      .append(POP(Seq(FP)))
+
+    if (isBranch) end.append(POP(Seq(PC)))
+
+    (start, end)
+  }
 }
