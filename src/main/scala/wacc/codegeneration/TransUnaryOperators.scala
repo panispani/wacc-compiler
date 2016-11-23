798488a0d9aa70296e8fb09718a45bbdb3736b2c
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2016-11-23 18:46:33 +0000

    Implement MinusOP translation

--- src/main/scala/wacc/codegeneration/TransUnaryOperators.scala
+++ src/main/scala/wacc/codegeneration/TransUnaryOperators.scala
@@ -8,6 +8,8 @@ package object TransUnaryOperators {
   def transUnaryOperator(r: Register, op: UnaryOperator): Seq[Instruction] = {
     op match {
       case LenOp  => Seq(LDR(r, RegisterAddress(r, 0)))
+
+      case MinusOp => Seq(RSBS(r, r, ImmOperand(0)))
     }
   }
 
