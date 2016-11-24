412cf3bd3be27ab59956aef2cdc23349c27a8c98
Tencho Tenev <tt1215@ic.ac.uk>
2016-11-24 01:51:18 +0000

    Make a note about cleaning up in case of failure
    See: http://stackoverflow.com/questions/30748064/in-bash-how-to-not-create-the-redirect-output-file-once-the-command-fails

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -10,6 +10,7 @@ SOURCE_FILE=$(basename ${SOURCE_PATH})
 ASM=${SOURCE_FILE%wacc}s
 EXE=${SOURCE_FILE%.wacc}
 
+# TODO: remove assembly file in case of compilation errors
 $(pwd)/compile ${SOURCE_PATH} > ${ASM} && \
 arm-linux-gnueabi-gcc -o ${EXE} -mcpu=arm1176jzf-s -mtune=arm1176jzf-s ${ASM} && \
 qemu-arm -L /usr/arm-linux-gnueabi/ ${EXE}
