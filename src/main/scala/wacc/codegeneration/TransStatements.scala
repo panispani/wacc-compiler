741ea08d2a345aac7c4cabed3c49190a36946417
Emanuele Rossi <er1115@point21.doc.ic.ac.uk>
2016-11-24 18:00:31 +0000

    Implement printing strings

--- src/main/scala/wacc/codegeneration/TransStatements.scala
+++ src/main/scala/wacc/codegeneration/TransStatements.scala
@@ -225,6 +225,7 @@ package object TransStatements {
       case Integer   => StaticCode.printIntLabel
       case Character => StaticCode.printCharLabel
       case Boolean   => StaticCode.printBoolLabel
+      case String    => StaticCode.printFunctionLabel
       case ArrayType(_) => StaticCode.printReferenceFunctionLabel
       case default   => StaticCode.printFunctionLabel
     }
@@ -242,6 +243,7 @@ package object TransStatements {
       case Integer   => StaticCode.printIntLabel
       case Character => StaticCode.printCharLabel
       case Boolean   => StaticCode.printBoolLabel
+      case String    => StaticCode.printFunctionLabel
       case ArrayType(_) => StaticCode.printReferenceFunctionLabel
       case default   => StaticCode.printFunctionLabel
     }
