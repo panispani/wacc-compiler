84fc87250458648f88efb8dfaa3811a5215f2b66
Tencho Tenev <tt1215@ic.ac.uk>
2016-11-24 02:08:50 +0000

    Make variable for compiler path in script

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -4,6 +4,8 @@
 # Creates an executable with a cross-compiler for ARM11
 # Emulates the created executable
 
+COMPILER_PATH=$(dirname $0)/compile
+
 SOURCE_PATH=$1
 SOURCE_FILE=$(basename ${SOURCE_PATH})
 
@@ -11,6 +13,6 @@ ASM=${SOURCE_FILE%wacc}s
 EXE=${SOURCE_FILE%.wacc}
 
 # TODO: remove assembly file in case of compilation errors
-$(pwd)/compile ${SOURCE_PATH} > ${ASM} && \
+${COMPILER_PATH} ${SOURCE_PATH} > ${ASM} && \
 arm-linux-gnueabi-gcc -o ${EXE} -mcpu=arm1176jzf-s -mtune=arm1176jzf-s ${ASM} && \
 qemu-arm -L /usr/arm-linux-gnueabi/ ${EXE}
