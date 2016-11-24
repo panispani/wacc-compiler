033ee5b5cf73012277def31f83814ea1e049906d
Tencho Tenev <tt1215@ic.ac.uk>
2016-11-24 01:45:06 +0000

    Add script for compiling and running a source file

---
+++ compile_and_run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Compiles a WACC file to ARM11 assembly
+# Creates an executable with a cross-compiler for ARM11
+# Emulates the created executable
+
+SOURCE_PATH=$1
+SOURCE_FILE=$(basename ${SOURCE_PATH})
+
+ASM=${SOURCE_FILE%wacc}s
+EXE=${SOURCE_FILE%.wacc}
+
+$(pwd)/compile ${SOURCE_PATH} > ${ASM} && \
+arm-linux-gnueabi-gcc -o ${EXE} -mcpu=arm1176jzf-s -mtune=arm1176jzf-s ${ASM} && \
+qemu-arm -L /usr/arm-linux-gnueabi/ ${EXE}
