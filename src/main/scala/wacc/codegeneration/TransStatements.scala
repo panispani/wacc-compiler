acc893f91914e5cd6a0018db9507d6f7f383d583
Norbert Podsadowski <norbo11@googlemail.com>
2016-11-25 01:02:35 +0000

    Started implementing reading into pair elements

--- src/main/scala/wacc/codegeneration/TransStatements.scala
+++ src/main/scala/wacc/codegeneration/TransStatements.scala
@@ -44,7 +44,7 @@ object TransStatements {
         => transScopeStatement(sequence, symbolTable, registers)
 
       case stat @ ReadStatement(_)
-        => transReadStatement(stat, registers)
+        => transReadStatement(stat, symbolTable, registers)
 
       case free: FreeStatement
         => transFreeStatement(free, symbolTable, registers)
@@ -52,7 +52,7 @@ object TransStatements {
   }
 
   def transFreeStatement(free: FreeStatement, symbolTable: SymbolTable, registers: Seq[Register]): Seq[Instruction] = {
-
+    Seq()
   }
 
   def transSkipStatement() : Seq[Instruction] = {
@@ -145,21 +145,23 @@ object TransStatements {
       .extend(endFrame).instructions
   }
 
-  def transReadStatement(read: ReadStatement, registers: Seq[Register]): Seq[Instruction] = {
-    val target: Integer = read.target match {
-      case vr: VariableReference => vr.offset
-      case pe: PairElement       => 0 //TODO: implement
-      case ae: ArrayElement      => 0 //TODO: implement
+  def transReadStatement(read: ReadStatement, symbolTable: SymbolTable, registers: Seq[Register]): Seq[Instruction] = {
+    val instructions: CodeSegment = read.target match {
+      case vr: VariableReference => CodeSegment().append(ADD(registers.head, FP, ImmOperand(vr.offset)))
+      case pe: PairElement       => CodeSegment().extend(TransAssignRhs.getPairElementPointer(pe, symbolTable, registers))
+      case ae: ArrayElement      => CodeSegment()
     }
 
-    val printLabel: Label = read.target.vartype match {
+    val readLabel: Label = read.target.vartype match {
       case Integer   => StaticCode.readIntLabel
       case Character => StaticCode.readCharLabel
     }
 
+
     CodeSegment()
-      .append(ADD(R0, FP, ImmOperand(target)))     // r0 = address of target
-      .append(BL(printLabel))                     // reads input into desired variable
+      .extend(instructions)
+      .append(MOV(R0, registers.head))
+      .append(BL(readLabel))
       .instructions
   }
 
