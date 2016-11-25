b9fbd2f89c83c8bdbbf60dcfc89bf7c78568a784
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2016-11-25 05:30:26 +0000

    Add case for pairtype in print

--- src/main/scala/wacc/codegeneration/TransStatements.scala
+++ src/main/scala/wacc/codegeneration/TransStatements.scala
@@ -188,6 +188,7 @@ object TransStatements {
       case Boolean   => StaticCode.printBoolLabel
       case String    => StaticCode.printFunctionLabel
       case ArrayType(_) => StaticCode.printReferenceFunctionLabel
+      case PairType(_, _) => StaticCode.printReferenceFunctionLabel
       case _         => StaticCode.printFunctionLabel
     }
 
