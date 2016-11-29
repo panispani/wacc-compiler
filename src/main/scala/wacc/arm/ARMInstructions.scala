9bb8d68eb5d57d6ce157c4ada17adabec23f2070
Emanuele Rossi <er1115@matrix42.doc.ic.ac.uk>
2016-11-29 23:03:55 +0000

    Remove ADDLSL instruction

--- src/main/scala/wacc/arm/ARMInstructions.scala
+++ src/main/scala/wacc/arm/ARMInstructions.scala
@@ -38,10 +38,6 @@ abstract class TwoRegOneOpInstruction extends ConditionalInstruction {
 
 case class ADD(Rd: Register, Rn: Register, Op1: Operand, cond: Condition = ALWAYS) extends TwoRegOneOpInstruction
 
-case class ADDLSL(Rd: Register, Rn: Register, Rm: Register, shift: Shift, cond: Condition = ALWAYS) extends Instruction {
-  override def toString = s"ADD $Rd, $Rn, $Rm, ${shift.toString}"
-}
-
 case class ADDS(Rd: Register, Rn: Register, Op1: Operand, cond: Condition = ALWAYS) extends TwoRegOneOpInstruction
 
 case class SUB(Rd: Register, Rn: Register, Op1: Operand, cond: Condition = ALWAYS) extends TwoRegOneOpInstruction
