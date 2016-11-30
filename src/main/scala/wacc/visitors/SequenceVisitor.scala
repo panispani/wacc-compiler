117f6bae9714650b1774f434e5b06b13a4d86e9c
panpan2 <p.panayiotou2@gmail.com>
2016-11-30 23:36:33 +0000

    Helper method for parsing sequences which are scopes

--- src/main/scala/wacc/visitors/SequenceVisitor.scala
+++ src/main/scala/wacc/visitors/SequenceVisitor.scala
@@ -2,14 +2,22 @@ package wacc.visitors
 
 import antlr.WACCParser.SequenceContext
 import antlr.WACCParserBaseVisitor
-import wacc.constructs._
+import wacc.SymbolTable
+import wacc.constructs.{CompilationError, _}
 
 import scala.collection.JavaConversions._
 
 object SequenceVisitor extends WACCParserBaseVisitor[Either[CompilationError, Seq[Statement]]] {
   override def visitSequence(ctx: SequenceContext): Either[CompilationError, Seq[Statement]] = {
-    for {
-      seq <- sequenceOrLast(ctx.statement() map (_.accept(StatementVisitor))).right
-    } yield seq
+    sequenceOrLast(ctx.statement() map (_.accept(StatementVisitor)))
+  }
+
+  def visitScopedSequence(ctx: SequenceContext): Either[CompilationError, ScopeStatement] = {
+    SymbolTable.openScope()
+    val statements = sequenceOrLast(ctx.statement().map(_.accept(StatementVisitor)))
+    val scope = statements.right.map(ScopeStatement(_, SymbolTable()))
+    SymbolTable.closeScope()
+
+    scope
   }
 }
