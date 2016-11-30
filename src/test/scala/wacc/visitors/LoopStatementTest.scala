bf388aa27c383ec21061a521a4d9b4ecb7260c8b
panpan2 <p.panayiotou2@gmail.com>
2016-11-30 21:10:43 +0000

    [TT][PP] Add test for doWhile statement

--- src/test/scala/wacc/visitors/LoopStatementTest.scala
+++ src/test/scala/wacc/visitors/LoopStatementTest.scala
@@ -16,4 +16,14 @@ class LoopStatementTest extends VisitorTest {
 
     loop.symbolTable.lookup("x") shouldBe defined
   }
+
+  "A do-while statement" should "be possible" in {
+    val parser = TestUtilities.setupParser("int x = 1; do int x = 1 while true")
+    val result = TestUtilities.buildSubProgram(parser.sequence, SequenceVisitor)
+
+    result.right.value(1) should be (a[LoopStatement])
+    val loop = result.right.value(1).asInstanceOf[LoopStatement]
+
+    loop.doWhile shouldBe true
+  }
 }
