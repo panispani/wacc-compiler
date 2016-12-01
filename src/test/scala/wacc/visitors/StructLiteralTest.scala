10726e8baa3c64a89d334a5a96e60a580426b5c8
Emanuele Rossi <er1115@matrix44.doc.ic.ac.uk>
2016-12-01 18:36:28 +0000

    Implement test for struct literal

---
+++ src/test/scala/wacc/visitors/StructLiteralTest.scala
@@ -0,0 +1,22 @@
+package wacc.visitors
+
+import wacc.constructs._
+import wacc.{TestUtilities, VariableReference}
+
+class StructLiteralTest extends VisitorTest {
+
+  "Assigning a struct literal" should "be possible" in {
+    val parser = TestUtilities.setupParser("{1, 2}")
+    val result = TestUtilities.buildSubProgram(parser.structLiteral, StructLiteralVisitor)
+
+    result.right.value should be (
+            StructLiteral(
+              List(
+                IntegerLiteral(1),
+                IntegerLiteral(2)
+              )
+            )
+        )
+  }
+
+}
