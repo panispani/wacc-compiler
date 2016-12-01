f1c5cb4b85315aba0bf9a8d5dd28f5e175ba14e2
Tencho Tenev <tt1215@imperial.ac.uk>
2016-12-01 11:15:49 +0000

    Init of for loop is always declare statement - syntax error otherwise

--- src/main/scala/wacc/constructs/Statement.scala
+++ src/main/scala/wacc/constructs/Statement.scala
@@ -181,6 +181,6 @@ case class LoopStatement(condition: Expression, statements: Seq[Statement], symb
   }
 }
 
-case class ForLoopStatement(init: Statement, cond: Expression, step: Statement, body: Seq[Statement]) extends Statement {
+case class ForLoopStatement(init: DeclareStatement, cond: Expression, step: Statement, body: Seq[Statement]) extends Statement {
   override def transStatement(registers: Seq[Register]): CodeSegment = CodeSegment()
 }
