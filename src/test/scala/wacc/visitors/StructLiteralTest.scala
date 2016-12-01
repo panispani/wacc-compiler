9e99dba765585c9ff2c96dddbd1cc0cd2ef559a5
Emanuele Rossi <er1115@matrix44.doc.ic.ac.uk>
2016-12-01 18:37:24 +0000

    Refactor test for struct literal

--- src/test/scala/wacc/visitors/StructLiteralTest.scala
+++ src/test/scala/wacc/visitors/StructLiteralTest.scala
@@ -10,13 +10,13 @@ class StructLiteralTest extends VisitorTest {
     val result = TestUtilities.buildSubProgram(parser.structLiteral, StructLiteralVisitor)
 
     result.right.value should be (
-            StructLiteral(
-              List(
-                IntegerLiteral(1),
-                IntegerLiteral(2)
-              )
-            )
+        StructLiteral(
+          List(
+            IntegerLiteral(1),
+            IntegerLiteral(2)
+          )
         )
+    )
   }
 
 }
