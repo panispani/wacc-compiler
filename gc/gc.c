cdf24635c574cacc2209d3fbe0eee5c58c1c7e56
panpan2 <p.panayiotou2@gmail.com>
2016-12-02 15:17:56 +0000

    gc rough idea

---
+++ gc/gc.c
@@ -0,0 +1,19 @@
+
+// in use means referenced by a variable in scope
+// or the object referenced by another object that is in use
+// make a graph of reachable objects
+// mark and sweep
+// traverse entire object graph, mark each one as reachable
+// traverse heap and delete objects that are unreachable
+
+void gc_malloc() {
+	
+}
+
+void gc() {
+	
+}
+
+void gc_free() {
+	// gc_free is not needed for now
+}
