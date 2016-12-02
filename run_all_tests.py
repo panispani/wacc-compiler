b251558e5c9cfb720f29e63741eb5f0f81e8bbae
Norbert Podsadowski <norbo11@googlemail.com>
2016-12-02 20:55:00 +0000

    Fixed status codes in test script

--- run_all_tests.py
+++ run_all_tests.py
@@ -20,6 +20,8 @@ for root, dirs, files in os.walk("wacc_examples"):
 
         (ref_status, ref_out) = commands.getstatusoutput("wacc_examples/refCompile -x {} < {}".format(full_path, input_file))
         (our_status, our_out) = commands.getstatusoutput("./execute.sh {} < {}".format(full_path, input_file))
+        our_status = os.WEXITSTATUS(our_status)
+        ref_status = -1
 
         if (os.path.isfile(os.path.splitext(file)[0] + ".s")):
             os.remove(os.path.splitext(file)[0] + ".s")
@@ -31,7 +33,11 @@ for root, dirs, files in os.walk("wacc_examples"):
         program_output = ""
         outputting = False
 
+        exit_start = "Errors detected during compilation! Exit code "
         for line in ref_out:
+            if line.startswith(exit_start):
+                ref_status = int(line.split(exit_start)[1].split(" ")[0])
+
             if line == "===========================================================":
                 outputting = not outputting
                 continue
@@ -41,26 +47,26 @@ for root, dirs, files in os.walk("wacc_examples"):
 
         program_output = program_output[:-1]
 
-        if ref_status == our_status:
-            if program_output == our_out:
-                correct = correct + 1
-            else:
-                print("=================OUTPUT MISMATCH===========================")
-                print("Theirs (Length {}):".format(len(program_output)))
-                print(program_output)
-                print("Ours (Length {}):".format(len(our_out)))
-                print(our_out)
-                print("================/OUTPUT MISMATCH===========================")
-        else:
+        if root.startswith("wacc_examples/invalid") and ref_status != our_status:
             print("==================STATUS MISMATCH==========================")
             print("Theirs: {}".format(ref_status))
             print("Ours  : {}".format(our_status))
             print("==================/STATUS MISMATCH==========================")
+        elif root.startswith("wacc_examples/valid") and program_output != our_out:
+            print("=================OUTPUT MISMATCH===========================")
+            print("Theirs (Length {}):".format(len(program_output)))
+            print(program_output)
+            print("Ours (Length {}):".format(len(our_out)))
+            print(our_out)
+            print("================/OUTPUT MISMATCH===========================")
+        else:
+             correct = correct + 1
 
         total = total + 1
 
 print("TEST CASES PASSED: {}/{} ({:.2f}%)".format(correct, total, correct / total * 100.0))
 
-
+if correct != total:
+    sys.exit(1)
 
 
