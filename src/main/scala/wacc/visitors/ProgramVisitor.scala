a0f7b3f3a817f08f1786bd8342700b480c2a23b7
Emanuele Rossi <er1115@ray12.doc.ic.ac.uk>
2016-12-03 15:06:31 +0000

    Visit structs in ProgramVisitor

--- src/main/scala/wacc/visitors/ProgramVisitor.scala
+++ src/main/scala/wacc/visitors/ProgramVisitor.scala
@@ -1,14 +1,28 @@
 package wacc.visitors
 
-import antlr.WACCParser.{FunctionContext, ProgramContext}
+import antlr.WACCParser.{FunctionContext, ProgramContext, StructContext}
 import antlr.WACCParserBaseVisitor
 import wacc.constructs._
-import wacc.{FunctionReference, SymbolTable, VariableReference}
+import wacc.{FunctionReference, SymbolTable}
 
 import scala.collection.JavaConversions._
 
 object ProgramVisitor extends WACCParserBaseVisitor[Either[Seq[CompilationError], Program]] {
 
+  private def defineStruct(ctx: StructContext): Option[SemanticError] = {
+
+    val name = ctx.IDENT().getText
+
+    // Check for duplicate struct name
+    if (SymbolTable.structsTable contains name)
+      return Some(SemanticError("Attempted redefinition of struct " + name, ctx.start))
+
+    SymbolTable.declareStruct(Struct(name, ctx.structMember().toList map (_.accept(StructMemberVisitor))))
+
+    None
+  }
+
+  //Todo: Join (or at least separate better and remove duplication) with FunctionVisitor
   private def defineFunction(ctx: FunctionContext): Option[SemanticError] = {
 
     val name = ctx.IDENT().getText
@@ -47,6 +61,15 @@ object ProgramVisitor extends WACCParserBaseVisitor[Either[Seq[CompilationError]
       case statement: Statement => Right(statement)
     }
 
+    //define structs
+    ctx.struct() foreach (s => {
+      defineStruct(s) match {
+        case Some(SemanticError(error, symbol)) =>
+          return Left(Seq(SemanticError(error, symbol)))
+        case None =>
+      }
+    })
+
     //define functions
     ctx.function() foreach (f => {
       defineFunction(f) match {
@@ -57,9 +80,10 @@ object ProgramVisitor extends WACCParserBaseVisitor[Either[Seq[CompilationError]
     })
 
     for {
+      structs <- sequenceOrAll(ctx.struct().toList map (e => e.accept(StructVisitor))).right
       functions <- sequenceOrAll(ctx.function().toList map (e => e.accept(FunctionVisitor))).right
       statements <- sequenceOrAll(ctx.sequence().statement().toList map (
         _.accept(StatementVisitor).right.flatMap(semanticErrorIfReturn))).right
-    } yield Program(functions, ScopeStatement(statements, SymbolTable.globalTable))
+    } yield Program(structs, functions, ScopeStatement(statements, SymbolTable.globalTable))
   }
 }
