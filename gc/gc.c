2ae71895e8564350ae25c55e5d0ca67df364f293
panpan2 <p.panayiotou2@gmail.com>
2016-12-03 13:46:54 +0000

    Allocate correct amount of memory for each object and initialise one as an example

--- gc/gc.c
+++ gc/gc.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 
 // in use means referenced by a variable in scope
 // or the object referenced by another object that is in use
@@ -21,38 +22,38 @@ typedef enum {
 typedef struct _object {
 	Object_type type;
 	unsigned char marked; // 1 byte, bool is 4 bytes
-	
+
 	union fields {
 		// INT
 		int value;
-		
+
 		// CHAR
-		char value;
-		
+		char chr;
+
 		// PAIR
 		struct {
 			struct _object *first;
 			struct _object *second;
 		};
-		
+
 		// STRING
 		struct {
 			struct _object *ch;    // char, last one is '\0'
 			struct _object *next; // a pair
 		};
-		
+
 		// ARRAY - will think about it
-		
+
 		// STRUCT
 		struct {
-			struct _object *list; // list of pairs, each pair having an element and next pair
+			struct _object *structlist; // list of pairs, each pair having an element and next pair
 		};
-		
+
 		// CLASS
 		struct {
-			struct _object *list; // list of pairs, each pair having an element and next pair
+			struct _object *classlist; // list of pairs, each pair having an element and next pair
 		};
-		
+
 	};
 } Object;
 
@@ -62,7 +63,7 @@ typedef struct {
 	// list of object pointers, until malloc fails
 	Object** stack;
 	// head of object list
-	Object* head; 
+	Object* head;
 	// current number of objects
 	int num_objects;
 } VM;
@@ -70,17 +71,16 @@ typedef struct {
 
 /************ FUNCTIONS *****************/
 
-//todo
 static VM* newVM() {
 	  VM* vm = malloc(sizeof(VM));
 	  vm->num_objects = 0;
 	  vm->head = NULL;
-	  // add dummy head to stack?
+	  // add dummy head to stack? YES we now get null pointer excpection
 	  return vm;
 }
 
 static void gc() {
-	
+
 }
 
 // heuristics of when to call GC
@@ -93,26 +93,30 @@ static Object* new_object(VM* vm, int type, int stackbytes, int refbytes) {
 	if (should_gc()) {
 		gc();
 	}
-	Object* object = malloc();
+	// rethink
+	Object* object = malloc(stackbytes + refbytes);
 	object->type = type;
 	object->marked = 0;
 	// append to front of VM object list
-	object->next = vm->next;
+	object->next = vm->head->next;
 	vm->head = object;
 	vm->num_objects = vm->num_objects + 1;
 	return object;
 }
 
 
-
-
 // only call this function
-void gc_malloc(int type, int stackbytes, int refbytes) {
-	static VM vm = newVM();
+void* gc_malloc(int type, int stackbytes, int refbytes) {
+	VM* vm = newVM(); // TODO make static
 	Object *object = new_object(vm, type, stackbytes, refbytes);
+	return (void*)object;
 	// pop from vm stack the refbytes in object list
-	// push(vm, object);
 }
 
 // gc_free is not needed for now
 void gc_free() {}
+
+int main() {
+    Object* object = gc_malloc(0, 4, 0);
+    return 0;
+}
