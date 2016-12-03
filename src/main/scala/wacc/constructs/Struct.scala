f295fe3700c5fee6d1da3d8556853a2a5b415209
Emanuele Rossi <er1115@ray12.doc.ic.ac.uk>
2016-12-03 14:32:52 +0000

    Add comment to describe Struct

--- src/main/scala/wacc/constructs/Struct.scala
+++ src/main/scala/wacc/constructs/Struct.scala
@@ -2,4 +2,7 @@ package wacc.constructs
 
 import wacc.{VariableReference}
 
+/**
+  * The members are variable references and their offset is respective to the start of the struct
+  */
 case class Struct(identifier: String, members: Seq[VariableReference])
