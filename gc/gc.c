019ec01096eef7c50a992a0ce177519815d01bb4
panpan2 <p.panayiotou2@gmail.com>
2016-12-04 11:40:52 +0000

    Mark for GC logic, other changes due to windows ^M ..

--- gc/gc.c
+++ gc/gc.c
@@ -17,55 +17,55 @@
 // do integers even need to be in the VM stack? NO, they
 // actually populate it even when we exit scope, TODO
 typedef enum {
-	INT,
-	CHAR,
-	PAIR,
-	STRING,
-	ARRAY,
-	STRUCT,
-	CLASS
+        INT,
+        CHAR,
+        PAIR,
+        STRING,
+        ARRAY,
+        STRUCT,
+        CLASS
 } Object_type;
 
 typedef struct _object {
-	Object_type type;
-	struct _object *next; // next object in VM stack
-	unsigned char marked; // 1 byte, bool is 4 bytes
+        Object_type type;
+        struct _object *next; // next object in VM stack
+        unsigned char marked; // 1 byte, bool is 4 bytes
 
-	union {
-		// INT
-		int value;
+        union {
+                // INT
+                int value;
 
-		// CHAR
-		char chr;
+                // CHAR
+                char chr;
 
-		// PAIR
-		struct {
-			struct _object *first;
-			struct _object *second;
-		};
+                // PAIR
+                struct {
+                        struct _object *first;
+                        struct _object *second;
+                };
 
-		// STRING - maybe merge with ARRAY
-		struct {
-			struct _object *ch;    // char, last one is '\0'
-			struct _object *next; // a pair
-		};
+                // STRING - maybe merge with ARRAY
+                struct {
+                        struct _object *ch;    // char, last one is '\0'
+                        struct _object *next; // a pair
+                };
 
-		// ARRAY
-		struct {
-			struct _object *array;
-		};
+                // ARRAY
+                struct {
+                        struct _object *array;
+                };
 
-		// STRUCT
-		struct {
-			struct _object *structlist; // list of pairs, each pair having an element and next pair
-		};
+                // STRUCT
+                struct {
+                        struct _object *structlist; // list of pairs, each pair having an element and next pair
+                };
 
-		// CLASS
-		struct {
-			struct _object *classlist; // list of pairs, each pair having an element and next pair
-		};
+                // CLASS
+                struct {
+                        struct _object *classlist; // list of pairs, each pair having an element and next pair
+                };
 
-	} fields;
+        } fields;
 } Object;
 
 // may keep also a field of when to trigger a GC
@@ -73,13 +73,13 @@ typedef struct _object {
 //TODO how do i remove ariables with scope? I rewrite old ones :)
 #define MAX_STACK_SIZE 4096
 typedef struct {
-	// array of object pointers
-	Object* stack[MAX_STACK_SIZE];
-	int stack_size;
-	// head of object list
-	Object* head;
-	// current number of objects
-	int num_objects;
+        // array of object pointers
+        Object* stack[MAX_STACK_SIZE];
+        int stack_size;
+        // head of object list
+        Object* head;
+        // current number of objects
+        int num_objects;
 } VM;
 
 static VM* vm;
@@ -87,11 +87,22 @@ static VM* vm;
 /************ FUNCTIONS *****************/
 
 static VM* newVM() {
-	  VM* vm = (VM*)malloc(sizeof(VM));
-	  vm->num_objects = 0;
-	  vm->stack_size = 0;
-	  vm->head = NULL;
-	  return vm;
+          VM* vm = (VM*)malloc(sizeof(VM));
+          vm->num_objects = 0;
+          vm->stack_size = 0;
+          vm->head = NULL;
+          return vm;
+}
+
+static void markAll() {
+    // start marking from the stack allocated variables
+    for (int i = 0; i < vm->stack_size; i++) {
+        mark(vm->stack);
+    }
+}
+
+static void mark(Object* object) {
+    // if marked finish, recurse on childs
 }
 
 static void gc() {
@@ -100,8 +111,8 @@ static void gc() {
 
 // heuristics of when to call GC
 static int should_gc() {
-	// casted to false
-	return 0;
+        // casted to false
+        return 0;
 }
 
 
@@ -114,94 +125,94 @@ static int should_gc() {
 #define STRUCTTYPE 5
 
 static Object* new_object() {
-	if (should_gc()) {
-		gc();
-	}
-	// rethink
-	Object* object = (Object*)malloc(sizeof(Object));
-	if (object == NULL) {
-		printf("%s\n", "Stack overflow");
-		return object;
-	}
+        if (should_gc()) {
+                gc();
+        }
+        // rethink
+        Object* object = (Object*)malloc(sizeof(Object));
+        if (object == NULL) {
+                printf("%s\n", "Stack overflow");
+                return object;
+        }
 
-	object->marked = 0;
-	// append to front of VM object list
-	if (vm->head == NULL) {
+        object->marked = 0;
+        // append to front of VM object list
+        if (vm->head == NULL) {
       object->next = NULL;
-	} else {
+        } else {
       object->next = vm->head->next;
-	}
-	vm->head = object;
-	vm->num_objects = vm->num_objects + 1;
+        }
+        vm->head = object;
+        vm->num_objects = vm->num_objects + 1;
 
-	return object;
+        return object;
 }
 
 static void pushVM(Object* object, int id) {
-	// put on stack of VM
-	vm->stack[id] = object;
-	vm->stack_size++;
-	//stack_size == id?
+        // put on stack of VM
+        vm->stack[id] = object;
+        vm->stack_size++;
+        //stack_size == id?
 }
 
 // look on stack for object with corresponing id
 static Object* get_object_with_id(int id) {
-	if (id == -1) {
-		// not a reference type, shouldn't care
-		return NULL; // or int-singleton
-	}
-	return vm->stack[id];
+        if (id == -1) {
+                // not a reference type, shouldn't care
+                return NULL; // or int-singleton
+        }
+        return vm->stack[id];
 }
 
 /*** PAIR ***/
 Object* declare_pair_constructor(int id, int val1_id, int val2_id) {
-	Object* object = new_object();
-	object->type = PAIR;
-	object->fields.first = get_object_with_id(val1_id);
-	object->fields.second = get_object_with_id(val2_id);
-	pushVM(object, id);
-	return object;
+        Object* object = new_object();
+        object->type = PAIR;
+        object->fields.first = get_object_with_id(val1_id);
+        object->fields.second = get_object_with_id(val2_id);
+        pushVM(object, id);
+        return object;
 }
 
 Object* declare_pair_copy(int id1, int id2) {
-	Object* object = new_object();
-	Object* copyfrom = get_object_with_id(id2);
-	object->type = PAIR;
-	object->fields = copyfrom->fields;
-	pushVM(object, id1);
-	return object;
+        Object* object = new_object();
+        Object* copyfrom = get_object_with_id(id2);
+        object->type = PAIR;
+        object->fields = copyfrom->fields;
+        pushVM(object, id1);
+        return object;
 }
 
 Object* assign_pair(int id1, int id2) {
-	Object* object = get_object_with_id(id2);
-	pushVM(object, id1);
-	return object;
+        Object* object = get_object_with_id(id2);
+        pushVM(object, id1);
+        return object;
 }
 
 /*** ARRAY ***/
 Object* declare_array_literal(int id, int size) {
-	Object* object = new_object();
-	object->type = ARRAY;
-	object->fields.array = (Object*)malloc(size * sizeof(Object));
-	pushVM(object, id);
-	return object;
+        Object* object = new_object();
+        object->type = ARRAY;
+        object->fields.array = (Object*)malloc(size * sizeof(Object));
+        pushVM(object, id);
+        return object;
 }
 
 // refactoring to be done - but NOT now
 Object* declare_array_copy(int id1, int id2) {
-	Object* object = new_object();
-	Object* copyfrom = get_object_with_id(id2);
-	object->type = ARRAY;
-	object->fields = copyfrom->fields;
-	pushVM(object, id1);
-	return object;
+        Object* object = new_object();
+        Object* copyfrom = get_object_with_id(id2);
+        object->type = ARRAY;
+        object->fields = copyfrom->fields;
+        pushVM(object, id1);
+        return object;
 }
 
 // refactoring to be done - but NOT now
 Object* assign_array(int id1, int id2) {
-	Object* object = get_object_with_id(id2);
-	pushVM(object, id1);
-	return object;
+        Object* object = get_object_with_id(id2);
+        pushVM(object, id1);
+        return object;
 }
 
 /*** STRING ***/
