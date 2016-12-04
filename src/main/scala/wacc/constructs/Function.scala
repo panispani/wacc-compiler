9480edc2f0a778316c0f0a771b976d505f530ab6
Norbert Podsadowski <norbo11@googlemail.com>
2016-12-04 21:10:54 +0000

    Added function to generate a unique string based on a function name + argument types

--- src/main/scala/wacc/constructs/Function.scala
+++ src/main/scala/wacc/constructs/Function.scala
@@ -4,4 +4,16 @@ import wacc.{SymbolTable, VariableReference}
 
 case class Function(identifier: String, params: Seq[VariableReference],
                     vartype: Type, statements: Seq[Statement],
-                    symbolTable: SymbolTable)
+                    symbolTable: SymbolTable) {
+
+  def getTypedName: String = Function.appendFunctionTypes(identifier, params map (_.vartype))
+}
+
+object Function {
+  def appendFunctionTypes(name: String, argumentTypes: Seq[Type]): String = {
+    if (argumentTypes.nonEmpty)
+      name + "_" + (argumentTypes map (t => t.toAssemblyLabel)).mkString("_")
+    else
+      name
+  }
+}
