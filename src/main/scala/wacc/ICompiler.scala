3ef2b779b6d9f2e2f856d1b7e58d4d8a30ea99a9
panpan2 <p.panayiotou2@gmail.com>
2016-12-05 13:53:59 +0000

    Refactor interactive compiler and handling of function case

--- src/main/scala/wacc/ICompiler.scala
+++ src/main/scala/wacc/ICompiler.scala
@@ -4,11 +4,33 @@ import java.io.ByteArrayInputStream
 
 import antlr.{WACCLexer, WACCParser}
 import org.antlr.v4.runtime.{ANTLRInputStream, CommonTokenStream}
-import wacc.arm.Registers
-import wacc.codegeneration.{CodeSegment, TransProgram, TransStatements}
-import wacc.visitors.{ProgramVisitor, StatementVisitor}
+import arm.Registers
+import codegeneration.{CodeSegment, TransFunctions, TransProgram, TransStatements}
+import constructs._
+import visitors.{FunctionVisitor, ProgramVisitor, StatementVisitor}
 
 object ICompiler extends App {
+
+  private def execStatement(tokens: CommonTokenStream): Either[CompilationError, Statement] = {
+    val parser = new WACCParser(tokens)
+    parser.addErrorListener(new ISyntaxErrorListener())
+    val tree = parser.statement()
+    if (!tree.isEmpty)
+      StatementVisitor.visit(tree)
+    else
+      Left(SyntaxError("It's not a statement", tree.start))
+  }
+
+  private def execFunction(tokens: CommonTokenStream): Either[CompilationError, Function] = {
+    val parser = new WACCParser(tokens)
+    parser.addErrorListener(new ISyntaxErrorListener())
+    val tree = parser.function()
+    if (!tree.isEmpty) {
+      FunctionVisitor.visit(tree)
+    }
+    else
+      Left(SyntaxError("It's not a function", tree.start))
+  }
 
   // make it loop for statements and functions
   while (true) {
@@ -16,16 +38,18 @@ object ICompiler extends App {
     val input = new ANTLRInputStream(new ByteArrayInputStream(inputStatement.getBytes()))
     val lexer = new WACCLexer(input)
     val tokens = new CommonTokenStream(lexer)
-    val parser = new WACCParser(tokens)
 
-    parser.addErrorListener(new SyntaxErrorListener())
-
-    val tree = parser.statement()
-    val statementAST = StatementVisitor.visit(tree)
-    statementAST match {
-      case Left(error) => error.raise()
-      case Right(stmt) => CodeSegment().extend(TransStatements.transStatement(stmt, Registers.expressionRegs)).release()
+    execStatement(tokens) match {
+      case Right(stmt) => {
+        CodeSegment().extend(TransStatements.transStatement(stmt, Registers.expressionRegs)).release()
+      }
+      case Left(error) => println("here"); println(tokens)
+        execFunction(tokens) match {
+          case Right(f) => println("asd"); println(f); CodeSegment().extend(TransFunctions.transFunction(f, Registers.expressionRegs)).release()
+          case Left(fError) => println("123");//fError.raise() ; error.raise() // TODO: only one of the two is needed
+        }
     }
+
   }
 
 }
