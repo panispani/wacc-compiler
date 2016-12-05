b5f0e49ef89e37ad97726fba22d4750fdffab888
Tencho Tenev <tt1215@imperial.ac.uk>
2016-12-05 17:04:07 +0000

    Use common Either type pattern in defineFunction

--- src/main/scala/wacc/visitors/ProgramVisitor.scala
+++ src/main/scala/wacc/visitors/ProgramVisitor.scala
@@ -11,21 +11,14 @@ object ProgramVisitor extends WACCParserBaseVisitor[Either[Seq[CompilationError]
 
   override def visitProgram(ctx: ProgramContext): Either[Seq[CompilationError], Program] = {
 
-    def semanticErrorIfReturn(statement: Statement) : Either[SemanticError, Statement] = statement match {
+    def semanticErrorIfReturn(statement: Statement): Either[SemanticError, Statement] = statement match {
       case ReturnStatement(_) => Left(SemanticError("Return statement in main program", ctx.start))
       case statement: Statement => Right(statement)
     }
 
-    ctx.function() foreach (f => {
-      FunctionVisitor.defineFunction(f) match {
-        case Some(SemanticError(error, symbol)) =>
-          return Left(Seq(SemanticError(error, symbol)))
-        case None =>
-      }
-    })
-
     for {
-      functions <- sequenceOrAll(ctx.function().toList map (e => e.accept(FunctionVisitor))).right
+      functionContexts <- sequenceOrAll(ctx.function().toList map FunctionVisitor.defineFunction).right
+      functions <- sequenceOrAll(functionContexts map (e => e.accept(FunctionVisitor))).right
       statements <- sequenceOrAll(ctx.sequence().statement().toList map (
         _.accept(StatementVisitor).right.flatMap(semanticErrorIfReturn))).right
     } yield Program(functions, ScopeStatement(statements, SymbolTable.globalTable))
