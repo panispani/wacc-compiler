00f144e75e8d288ceba2d741dcd92edb905fbb44
panpan2 <p.panayiotou2@gmail.com>
2016-12-05 12:35:37 +0000

    Interactive compiler implementation, translate statement by statement

--- src/main/scala/wacc/ICompiler.scala
+++ src/main/scala/wacc/ICompiler.scala
@@ -1,5 +1,7 @@
 package wacc
 
+import java.io.ByteArrayInputStream
+
 import antlr.{WACCLexer, WACCParser}
 import org.antlr.v4.runtime.{ANTLRInputStream, CommonTokenStream}
 import wacc.arm.Registers
@@ -7,20 +9,23 @@ import wacc.codegeneration.{CodeSegment, TransProgram, TransStatements}
 import wacc.visitors.{ProgramVisitor, StatementVisitor}
 
 object ICompiler extends App {
-  val input = new ANTLRInputStream(System.in)
-  val lexer = new WACCLexer(input)
-  val tokens = new CommonTokenStream(lexer)
-  val parser = new WACCParser(tokens)
-
-  parser.addErrorListener(new SyntaxErrorListener())
 
   // make it loop for statements and functions
-  val tree = parser.statement()
-  val statementAST = StatementVisitor.visit(tree)
+  while (true) {
+    val inputStatement = scala.io.StdIn.readLine()
+    val input = new ANTLRInputStream(new ByteArrayInputStream(inputStatement.getBytes()))
+    val lexer = new WACCLexer(input)
+    val tokens = new CommonTokenStream(lexer)
+    val parser = new WACCParser(tokens)
 
-  statementAST match {
-    case Left(error) => error.raise()
-    case Right(stmt) => CodeSegment().extend(TransStatements.transStatement(stmt, Registers.expressionRegs)).release()
+    parser.addErrorListener(new SyntaxErrorListener())
+
+    val tree = parser.statement()
+    val statementAST = StatementVisitor.visit(tree)
+    statementAST match {
+      case Left(error) => error.raise()
+      case Right(stmt) => CodeSegment().extend(TransStatements.transStatement(stmt, Registers.expressionRegs)).release()
+    }
   }
 
 }
