4f6e8a9c4fa7f58a88a0df248f356a1202c1f8d7
Norbert Podsadowski <norbo11@googlemail.com>
2016-12-06 19:02:39 +0000

    Changed backend to use new GC allocation method for pairs

--- src/main/scala/wacc/constructs/PairConstructor.scala
+++ src/main/scala/wacc/constructs/PairConstructor.scala
@@ -21,6 +21,20 @@ case class PairConstructor(firstExp: Expression, secondExp: Expression) extends 
     }
 
     CodeSegment()
+        .extend(BL(Label("new_pair_constructor")))           // R0: pair *newpair = new_pair_constructor
+        .extend(MOV(registers.head, R0))                     // return register = newpair
+        .extend(LDR(R0, RegisterAddress(R0)))                // R0 = *newpair (first elem address)
+        .extend(firstExp.transAssignRhs(registers.tail))     // next free reg = expression1
+        .extend(store1)                                      // Store the value for the first element in its memory
+        .extend(LDR(R0, RegisterAddress(registers.head, 4))) // R0 = *(newpair + 4)
+        .extend(secondExp.transAssignRhs(registers.tail))    // next free reg = expression2
+        .extend(store2)                                      // Store the value for the first element in its memory
+
+    /*
+    Commented out for the purposes of GC.
+    TODO Make an if to check for -gc flag when compiling
+
+    CodeSegment()
       .extend(LDR(R0, Const(8)))                           // Load the size of the pair in R0
       .extend(BL(Label("malloc")))
       .extend(MOV(registers.head, R0))
@@ -34,5 +48,6 @@ case class PairConstructor(firstExp: Expression, secondExp: Expression) extends 
       .extend(BL(Label("malloc")))
       .extend(store2)                                      // Store the value for the second element in its memory
       .extend(STR(R0, RegisterAddress(registers.head, 4))) // Put address of second element in memory of pair with offset
+    */
   }
 }
