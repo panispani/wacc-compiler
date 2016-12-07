1d755e8830301e67bee31e82719b7aa95fc03860
Emanuele Rossi <er1115@line34.doc.ic.ac.uk>
2016-12-07 16:41:17 +0000

    Change struct member to have a list of members

--- src/main/scala/wacc/constructs/StructMember.scala
+++ src/main/scala/wacc/constructs/StructMember.scala
@@ -2,18 +2,40 @@ package wacc.constructs
 
 import wacc.{SymbolTable, VariableReference}
 
-case class StructMember(struct: VariableReference, membersName: Seq[String]) extends Expression {
+case class StructMember(vr: VariableReference, membersName: Seq[String]) extends Expression {
 
   override val varType: Type = {
-    var currentStruct = struct
+    var currentStruct = vr
     for (memberName <- membersName) {
       currentStruct.varType match {
         case StructType(structId, _) => {
-          currentStruct = SymbolTable.structsTable(structId).members.filter(vr => vr.name == memberName).head
+          currentStruct = SymbolTable.structsTable(structId).members.find(vr => vr.name == memberName).get
         }
       }
     }
 
     currentStruct.varType
+  }
+
+  val membersOffset: Seq[Int] = {
+    var currentVr = vr
+    var result: Seq[Int] = Seq()
+
+    for (memberName <- membersName) {
+      currentVr.varType match {
+        case StructType(structId, _) => {
+             val s = SymbolTable.structsTable(structId)
+              val memberOffset = s.members
+                .takeWhile(m => m.name != membersName.last)
+                .map(m => m.varType.size)
+                .sum
+              result = result :+ memberOffset
+
+          currentVr = SymbolTable.structsTable(structId).members.find(vr => vr.name == memberName).get
+        }
+      }
+    }
+
+    result
   }
 }
