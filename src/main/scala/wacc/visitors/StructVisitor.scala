ba7de4b8ef40972ce770b85ff5c977b7b881e8c2
Emanuele Rossi <er1115@line34.doc.ic.ac.uk>
2016-12-07 16:59:19 +0000

    Mover struct member declaration visit inside struct visit

--- src/main/scala/wacc/visitors/StructVisitor.scala
+++ src/main/scala/wacc/visitors/StructVisitor.scala
@@ -2,6 +2,7 @@ package wacc.visitors
 
 import antlr.WACCParser.StructContext
 import antlr.WACCParserBaseVisitor
+import wacc.VariableReference
 import wacc.constructs.{CompilationError, Struct}
 
 import scala.collection.JavaConversions._
@@ -9,7 +10,14 @@ import scala.collection.JavaConversions._
 object StructVisitor extends WACCParserBaseVisitor[Either[CompilationError, Struct]] {
 
   override def visitStruct(ctx: StructContext): Either[CompilationError, Struct] = {
-    val members = ctx.structMemberDeclaration().toList map (_.accept(StructMemberDeclarationVisitor))
+    var offset = 0
+
+    val members = ctx.structMemberDeclaration().toList map (x => {
+      val varType = x.`type`.accept(TypeVisitor)
+      val vr = VariableReference(x.IDENT().toString, varType, offset)
+      offset += varType.size
+      vr
+    })
 
     Right(Struct(ctx.IDENT().getText, members))
   }
