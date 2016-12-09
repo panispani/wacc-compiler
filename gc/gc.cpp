644bd8622600352f5ab4ac2f08b9ffefbe2f9ad5
Norbert Podsadowski <norbo11@googlemail.com>
2016-12-09 01:53:03 +0000

    Improvements to gc_script

--- gc/gc.cpp
+++ gc/gc.cpp
@@ -207,20 +207,20 @@ void gc_free(void* obj) {
 }
 
 void collect_garbage() {
-    printf("\n---------- Before VM heap ---------\n");
-    for(auto obj : vm->heap) {
-        cout << obj.first << " -> " << obj.second << " of type " << obj.second->getType() << endl;
-    }
-    printf("--------------------\n");
+    //printf("\n---------- Before VM heap ---------\n");
+    //for(auto obj : vm->heap) {
+    //    cout << obj.first << " -> " << obj.second << " of type " << obj.second->getType() << endl;
+    // }
+    //printf("--------------------\n");
 
     markAll();
     sweep();
 
-    printf("\n---------- After VM heap ---------\n");
-    for(auto obj : vm->heap) {
-        cout << obj.first << " -> " << obj.second << " of type " << obj.second->getType() << endl;
-    }
-    printf("--------------------\n");
+    //printf("\n---------- After VM heap ---------\n");
+    //for(auto obj : vm->heap) {
+    //    cout << obj.first << " -> " << obj.second << " of type " << obj.second->getType() << endl;
+    //}
+    //printf("--------------------\n");
 
 }
 
--- gc_script.sh
+++ gc_script.sh
@@ -6,10 +6,9 @@ SOURCE_FILE=$(basename ${SOURCE_PATH})
 
 ASM=${SOURCE_FILE%wacc}s
 
-${COMPILER_PATH} ${SOURCE_PATH}
-
-arm-linux-gnueabi-g++ -c -mcpu=arm1176jzf-s -mtune=arm1176jzf-s -std=c++11 gc/gc.cpp -o gc.o
-arm-linux-gnueabi-g++ -c -mcpu=arm1176jzf-s -mtune=arm1176jzf-s -std=c++11 gc/objects.cpp -o objects.o
-arm-linux-gnueabi-g++ -c -o asm.o ${ASM} -mcpu=arm1176jzf-s -mtune=arm1176jzf-s
-arm-linux-gnueabi-g++ -mcpu=arm1176jzf-s -mtune=arm1176jzf-s -o output gc.o asm.o objects.o
+${COMPILER_PATH} ${SOURCE_PATH} && \
+arm-linux-gnueabi-g++ -c -mcpu=arm1176jzf-s -mtune=arm1176jzf-s -std=c++11 gc/gc.cpp -o gc.o && \
+arm-linux-gnueabi-g++ -c -mcpu=arm1176jzf-s -mtune=arm1176jzf-s -std=c++11 gc/objects.cpp -o objects.o && \
+arm-linux-gnueabi-g++ -c -o asm.o ${ASM} -mcpu=arm1176jzf-s -mtune=arm1176jzf-s && \
+arm-linux-gnueabi-g++ -mcpu=arm1176jzf-s -mtune=arm1176jzf-s -o output gc.o asm.o objects.o && \
 qemu-arm -L /usr/arm-linux-gnueabi/ output
